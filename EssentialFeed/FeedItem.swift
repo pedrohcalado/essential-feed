5e58c0dfd2c353a1a7cd7d50ed63f80ed7cd8993
Pedro Calado <cons721@modal.net.br>
2022-02-17 17:23:15 -0300

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by PEDRO HENRIQUE CALADO on 2/17/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
