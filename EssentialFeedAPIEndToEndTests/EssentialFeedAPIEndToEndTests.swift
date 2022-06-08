465619eee3fe186824315b0d232f23a97600fde5
Pedro Calado <pedrocalado22@gmail.com>
2022-06-08 15:43:08 -0300

    Increase wait time in the expectation

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -47,7 +47,7 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
             receivedResult = result
             exp.fulfill()
         }
-        wait(for: [exp], timeout: 5.0)
+        wait(for: [exp], timeout: 10.0)
         return receivedResult
     }
 
