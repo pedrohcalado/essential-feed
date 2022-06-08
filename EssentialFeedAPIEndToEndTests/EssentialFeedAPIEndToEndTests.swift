e18d0dc91a23b7fe87017715ce97becfba34386d
Pedro Calado <pedrocalado22@gmail.com>
2022-06-08 12:38:21 -0300

    Extract test setup to a helper method

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -11,21 +11,9 @@ import EssentialFeed
 class EssentialFeedAPIEndToEndTests: XCTestCase {
 
     func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
-        let client = URLSessionHTTPClient()
-        let testServerURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5c52cdd0b8a045df091d2fff/1548930512083/feed-case-study-test-api-feed.json")!
-        let loader = RemoteFeedLoader(url: testServerURL, client: client)
         
-        let exp = expectation(description: "Wait for load completion")
         
-        var receivedResult: LoadFeedResult?
-        
-        loader.load { result in
-            receivedResult = result
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 5.0)
-        
-        switch receivedResult {
+        switch getFeedResult() {
         case let .success(items)?:
             XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
             XCTAssertEqual(items[0], expectedItem(at: 0))
@@ -43,59 +31,72 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
             XCTFail("Expected successful feed result, got no result instead")
         }
     }
+    
+    // MARK: - Helpers
+    private func getFeedResult() -> LoadFeedResult? {
+        let client = URLSessionHTTPClient()
+        let testServerURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5c52cdd0b8a045df091d2fff/1548930512083/feed-case-study-test-api-feed.json")!
+        let loader = RemoteFeedLoader(url: testServerURL, client: client)
+        
+        let exp = expectation(description: "Wait for load completion")
+        var receivedResult: LoadFeedResult?
+        
+        loader.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 5.0)
+        return receivedResult
+    }
+
+    private func expectedItem(at index: Int) -> FeedItem {
+        return FeedItem(
+            id: id(at: index),
+            description: description(at: index),
+            location: location(at: index),
+            imageURL: imageURL(at: index))
+    }
+
+    private func id(at index: Int) -> UUID {
+            return UUID(uuidString: [
+                "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
+                "BA298A85-6275-48D3-8315-9C8F7C1CD109",
+                "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
+                "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
+                "DC97EF5E-2CC9-4905-A8AD-3C351C311001",
+                "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
+                "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
+                "F79BD7F8-063F-46E2-8147-A67635C3BB01"
+            ][index])!
+        }
+
+        private func description(at index: Int) -> String? {
+            return [
+                "Description 1",
+                nil,
+                "Description 3",
+                nil,
+                "Description 5",
+                "Description 6",
+                "Description 7",
+                "Description 8"
+            ][index]
+        }
+
+        private func location(at index: Int) -> String? {
+            return [
+                "Location 1",
+                "Location 2",
+                nil,
+                nil,
+                "Location 5",
+                "Location 6",
+                "Location 7",
+                "Location 8"
+            ][index]
+        }
+
+        private func imageURL(at index: Int) -> URL {
+            return URL(string: "https://url-\(index+1).com")!
+        }
 }
-
-// MARK: - Helpers
-
-private func expectedItem(at index: Int) -> FeedItem {
-    return FeedItem(
-        id: id(at: index),
-        description: description(at: index),
-        location: location(at: index),
-        imageURL: imageURL(at: index))
-}
-
-private func id(at index: Int) -> UUID {
-        return UUID(uuidString: [
-            "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
-            "BA298A85-6275-48D3-8315-9C8F7C1CD109",
-            "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
-            "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
-            "DC97EF5E-2CC9-4905-A8AD-3C351C311001",
-            "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
-            "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
-            "F79BD7F8-063F-46E2-8147-A67635C3BB01"
-        ][index])!
-    }
-
-    private func description(at index: Int) -> String? {
-        return [
-            "Description 1",
-            nil,
-            "Description 3",
-            nil,
-            "Description 5",
-            "Description 6",
-            "Description 7",
-            "Description 8"
-        ][index]
-    }
-
-    private func location(at index: Int) -> String? {
-        return [
-            "Location 1",
-            "Location 2",
-            nil,
-            nil,
-            "Location 5",
-            "Location 6",
-            "Location 7",
-            "Location 8"
-        ][index]
-    }
-
-    private func imageURL(at index: Int) -> URL {
-        return URL(string: "https://url-\(index+1).com")!
-    }
-
-
