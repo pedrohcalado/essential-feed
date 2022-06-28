71f52ad2b6c9ed709f9280cdb7e6f0ed0ec19363
Pedro Calado <pedrocalado22@gmail.com>
2022-06-28 16:09:17 -0300

    CoreDataFeedStore.retrieve() delivers empty on empty cache

---
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -0,0 +1,25 @@
+//
+//  CoreDataFeedStore.swift
+//  EssentialFeed
+//
+//  Created by Pedro Henrique Calado on 28/06/22.
+//
+
+import Foundation
+
+public final class CoreDataFeedStore: FeedStore {
+    public init() {}
+
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.empty)
+    }
+
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+
+    }
+
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+
+    }
+
+}
--- EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -6,10 +6,13 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = makeSUT()
 
+        assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
     }
 
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
@@ -32,7 +35,7 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
 
     }
 
-    func test_insert_overridesPreviouslyInsertedCacheValues() {
+    func test_insert_overridesPreviousllyInsertedCacheValues() {
 
     }
 
@@ -54,6 +57,14 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
 
     func test_storeSideEffects_runSerially() {
 
+    }
+    
+    // - MARK: Helpers
+        
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
+        let sut = CoreDataFeedStore()
+        trackForMemoryLeaks(instance: sut, file: file, line: line)
+        return sut
     }
 
 }
