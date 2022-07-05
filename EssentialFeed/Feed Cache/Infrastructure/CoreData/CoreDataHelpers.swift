fae6f97cb408a50eed69cc03ce61b7ee869388d7
Pedro Calado <pedrocalado22@gmail.com>
2022-07-05 07:25:53 -0300

    Remove redundant internal access control declaration

--- EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataHelpers.swift
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataHelpers.swift
@@ -7,7 +7,7 @@
 
 import CoreData
 
-internal extension NSPersistentContainer {
+ extension NSPersistentContainer {
     enum LoadingError: Swift.Error {
         case modelNotFound
         case failedToLoadPersistentStores(Swift.Error)
@@ -29,7 +29,7 @@ internal extension NSPersistentContainer {
     }
 }
 
-internal extension NSManagedObjectModel {
+ extension NSManagedObjectModel {
     static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
         return bundle
             .url(forResource: name, withExtension: "momd")
