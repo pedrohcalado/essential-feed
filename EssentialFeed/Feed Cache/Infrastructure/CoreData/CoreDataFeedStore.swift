442d20b60464e37a2bab1db2e9782bbc4cce6476
Pedro Calado <pedrocalado22@gmail.com>
2022-07-06 07:28:13 -0300

    Simplify CoreDataFeedStore completion code with the new Result APIs

--- EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -1,64 +1,55 @@
-    //
-    //  CoreDataFeedStore.swift
-    //  EssentialFeed
-    //
-    //  Created by Pedro Henrique Calado on 28/06/22.
-    //
+//
+//  CoreDataFeedStore.swift
+//  EssentialFeed
+//
+//  Created by Pedro Henrique Calado on 28/06/22.
+//
 
-    import CoreData
+import CoreData
 
-    public final class CoreDataFeedStore: FeedStore {
-        private let container: NSPersistentContainer
-        private let context: NSManagedObjectContext
-
-        public init(storeURL: URL, bundle: Bundle = .main) throws {
-            container = try NSPersistentContainer.load(modelName: "FeedStore", url: storeURL, in: bundle)
-            context = container.newBackgroundContext()
-        }
-
-        public func retrieve(completion: @escaping RetrievalCompletion) {
-            perform { context in
-                do {
-                    if let cache = try ManagedCache.find(in: context) {                        completion(.success((CachedFeed(feed: cache.localFeed, timestamp: cache.timestamp))))
-                    } else {
-                        completion(.success(.none))
-                    }
-                } catch {
-                    completion(.failure(error))
-                }
-            }
-        }
-
-        public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-            perform { context in
-                do {
-                    let managedCache = try ManagedCache.newUniqueInstance(in: context)
-                    managedCache.timestamp = timestamp
-                    managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
-                    try context.save()
-                    completion(.success(()))
-                } catch {
-                    completion(.failure(error))
-                }
-            }
-        }
-        
-        public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-            perform { context in
-                do {
-                    try ManagedCache.find(in: context).map(context.delete).map(context.save)
-                    completion(.success(()))
-                } catch {
-                    completion(.failure(error))
-                }
-            }
-        }
-        
-        private func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
-            let context = self.context
-            context.perform { action(context) }
-        }
-
-    }
-
+public final class CoreDataFeedStore: FeedStore {
+    private let container: NSPersistentContainer
+    private let context: NSManagedObjectContext
     
+    public init(storeURL: URL, bundle: Bundle = .main) throws {
+        container = try NSPersistentContainer.load(modelName: "FeedStore", url: storeURL, in: bundle)
+        context = container.newBackgroundContext()
+    }
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        perform { context in
+            completion(Result {
+                try ManagedCache.find(in: context).map {
+                    return CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp)
+                }
+            })
+        }
+    }
+    
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        perform { context in
+            completion(Result {
+                let managedCache = try ManagedCache.newUniqueInstance(in: context)
+                managedCache.timestamp = timestamp
+                managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
+                try context.save()
+            })
+        }
+    }
+    
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        perform { context in
+            completion(Result {
+                try ManagedCache.find(in: context).map(context.delete).map(context.save)
+            })
+        }
+    }
+    
+    private func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
+        let context = self.context
+        context.perform { action(context) }
+    }
+    
+}
+
+
